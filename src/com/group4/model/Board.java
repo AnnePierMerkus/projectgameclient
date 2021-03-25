86d5256bcc73e51dad8cd26a3c899524b7debf6d
Gerwin <Gerwin_Terpstra@live.nl>
2021-03-25 16:55:51 +0100

    Board test

--- src/com/group4/model/Board.java
+++ src/com/group4/model/Board.java
@@ -1,7 +1,18 @@
 package com.group4.model;
 
-public class Board {
+import java.util.List;
 
+import com.group4.util.Tile;
+
+public class Board {
+	private List<Tile> gameBoard;
 	
-	
+	public void create(int height, int width) {
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				this.gameBoard.add(j, new Tile(i*j));
+			}
+		}
+	}
 }
+
--- src/com/group4/util/Tile.java
+++ src/com/group4/util/Tile.java
@@ -2,4 +2,7 @@ package com.group4.util;
 
 public class Tile {
 	
+	public Tile(int coordinate) {
+	}
+	
 }
