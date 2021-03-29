6b9e34e72e5f689d2656506268b72055ce96519c
Gerwin <Gerwin_Terpstra@live.nl>
2021-03-30 01:00:14 +0200

    Created the Observer/Observable Interfaces and concrete observer classes.

---
+++ src/com/group4/util/GameObserver.java
@@ -0,0 +1,15 @@
+package com.group4.util;
+
+import com.group4.observers.Observer;
+
+public class GameObserver implements Observer {
+
+	public GameObserver() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+}
--- src/com/group4/view/View.java
+++ src/com/group4/view/View.java
@@ -1,5 +1,45 @@
 package com.group4.view;
 
-public class View {
+import java.util.ArrayList;
+
+import com.group4.observers.Observable;
+import com.group4.observers.Observer;
+
+public class View implements Observable {
+	
+	// List of all the observers watching this view.
+	private ArrayList<Observer> viewObservers;
+	
+	public View() {
+		
+		// Initializing viewObservers with an empty ArrayList to contain Observer objects.
+		viewObservers = new ArrayList<Observer>();
+	}
+	
+	/**
+	 * Method that adds observers to the viewObserver list.
+	 */
+	@Override
+	public void registerObserver(Observer observer) {
+		viewObservers.add(observer);
+	}
+
+	/**
+	 * Method that removes observers from the viewObserver list.
+	 */
+	@Override
+	public void removeObserver(Observer observer) {
+		viewObservers.remove(observer);
+	}
+	
+	/**
+	 * Method that notifies all the observers in the viewObserver list that there is an update.
+	 */
+	@Override
+	public void notifyObservers() {
+		for(Observer observer : viewObservers) {
+			observer.update();
+		}
+	}
 
 }
