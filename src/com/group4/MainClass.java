64d9f131e4f3b59f6e6d6ed6be769d347346aafc
Gemar-k <gemar.koning@gmail.com>
2021-03-31 21:42:53 +0200

    testcode commented out

--- src/com/group4/MainClass.java
+++ src/com/group4/MainClass.java
@@ -15,6 +15,7 @@ public class MainClass extends Application {
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
+        /** network test code REMOVE IN FINAL VERSION
 		Client client = new Client("localhost", 7789);
 		Thread thread = new Thread(client);
 		client.registerObserver((Object object) -> {
@@ -26,6 +27,7 @@ public class MainClass extends Application {
 		NetworkPlayer networkPlayer = new NetworkPlayer("1010", "idea", new TICTACTOE(), client);
 		networkPlayer.login();
 		networkPlayer.getAvailableGames();
+         **/
 	}
 
 }
