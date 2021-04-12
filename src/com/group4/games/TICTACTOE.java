e218eed3895be2eac593a5bebebc8027a1d3f181
Gerwin <Gerwin_Terpstra@live.nl>
2021-04-12 17:04:34 +0200

    TicTacToe gameHasEnded update

--- src/com/group4/games/TICTACTOE.java
+++ src/com/group4/games/TICTACTOE.java
@@ -7,7 +7,9 @@ import java.util.List;
 import com.group4.controller.GameController.GameType;
 import com.group4.util.GameProperty;
 import com.group4.util.Player;
+import com.group4.util.PlayerList;
 import com.group4.util.Tile;
+import com.group4.util.Player.PlayerState;
 
 public class TICTACTOE extends GameProperty {
 	
@@ -58,21 +60,14 @@ public class TICTACTOE extends GameProperty {
 	@Override
 	public boolean makeMove(Tile tile, Player player) {
 		if(this.gameHasEnded()) {
-			System.out.println("Game has ended.");
 			return false;
 		}
-		System.out.println(player.getId());
-		System.out.println("Making move...");
 		if(tile != null) {
-			System.out.println("Selected tile: " + tile.getIndex());
 		}		
 		if(this.isLegalMove(tile, player)) {
-			System.out.println("Move legal");
 			tile.setOccupant(player);
-			this.endGameFlagMet(player);
 			return true;
 		}
-		System.out.println("Move illegal");
 		return false;
 	}
 
@@ -90,30 +85,27 @@ public class TICTACTOE extends GameProperty {
 	
 	@Override
 	public boolean gameHasEnded() {
-		return false;
-	}
-	
-	public boolean endGameFlagMet(Player player) {
 		ArrayList<Integer> playerTiles = new ArrayList<Integer>();
-		for(Tile tile : this.game.getBoard().getGameBoard().values()) {
-			if(tile.getOccupant() == player) {
-				playerTiles.add(tile.getIndex());
+		for(Player player : PlayerList.players.values()) {
+			for(Tile tile : this.game.getBoard().getGameBoard().values()) {
+				if(tile.getOccupant() == player) {
+					playerTiles.add(tile.getIndex());
+				}
 			}
-		}
-		if((playerTiles.contains(0) && playerTiles.contains(1) && playerTiles.contains(2)) ||
-		   (playerTiles.contains(3) && playerTiles.contains(4) && playerTiles.contains(5)) ||
-		   (playerTiles.contains(6) && playerTiles.contains(7) && playerTiles.contains(8)) ||
-		   (playerTiles.contains(0) && playerTiles.contains(3) && playerTiles.contains(6)) ||
-		   (playerTiles.contains(1) && playerTiles.contains(4) && playerTiles.contains(7)) ||
-		   (playerTiles.contains(2) && playerTiles.contains(5) && playerTiles.contains(8)) ||
-		   (playerTiles.contains(0) && playerTiles.contains(4) && playerTiles.contains(8)) ||
-		   (playerTiles.contains(2) && playerTiles.contains(4) && playerTiles.contains(6))
-		  ) {
-			this.playerWon = player;
-			this.gameEnded = true;
-			this.matchPoint = true;
-			System.out.println("Player " + player.getId() + " has won!");
-			return true;
+			if((playerTiles.contains(0) && playerTiles.contains(1) && playerTiles.contains(2)) ||
+			   (playerTiles.contains(3) && playerTiles.contains(4) && playerTiles.contains(5)) ||
+			   (playerTiles.contains(6) && playerTiles.contains(7) && playerTiles.contains(8)) ||
+			   (playerTiles.contains(0) && playerTiles.contains(3) && playerTiles.contains(6)) ||
+			   (playerTiles.contains(1) && playerTiles.contains(4) && playerTiles.contains(7)) ||
+			   (playerTiles.contains(2) && playerTiles.contains(5) && playerTiles.contains(8)) ||
+			   (playerTiles.contains(0) && playerTiles.contains(4) && playerTiles.contains(8)) ||
+			   (playerTiles.contains(2) && playerTiles.contains(4) && playerTiles.contains(6))
+			  ) {
+				this.playerWon = player;
+				this.gameEnded = true;
+				this.matchPoint = true;
+				return true;
+			}
 		}		
 		return false;
 	}
