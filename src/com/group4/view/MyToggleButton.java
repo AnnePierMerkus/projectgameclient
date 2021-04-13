16f5c213c34727d4dc0723cea5f9969e8607659f
Anne Pier Merkus <g.a.p.merkus@st.hanze.nl>
2021-04-13 12:14:17 +0200

    Javadoc

--- src/com/group4/view/MyToggleButton.java
+++ src/com/group4/view/MyToggleButton.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.ToggleButton;
 
 public class MyToggleButton extends ToggleButton {
+    /**
+     * Override click button so it doesn't become unselected and no checks are required.
+     */
     @Override
     public void fire() {
         if (!this.isDisabled() && !this.isSelected())
